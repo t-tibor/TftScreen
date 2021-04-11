a6065727270defa1b57f9885263ef6f592f28bfc
t-tibor <ttuti94@gmail.com>
2021-04-11 16:37:21 +0200

    Dummy LED blinker project with Microsoft Visual Code.

--- TftMain.c
+++ TftMain.c
@@ -1,11 +1,42 @@
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
 
+#include <avr/io.h>
+#include <util/delay.h>
 
-int main()
+#define md_LED_DDR_u8		DDRB
+#define md_LED_DDR_PIN_u8	DDB5
+#define md_LED_PORT_u8 		PORTB
+#define md_LED_PIN_u8		PORTB5
+#define md_WAIT_TIME_u32	((uint32_t)3000)
+
+static inline void m_InitLed_vd(void)
 {
-	int i = 3;
-	double t = 7.4;
+	md_LED_DDR_u8 |= _BV(md_LED_DDR_PIN_u8);
+}
 
-	i = t;
-	
+static inline void m_SetLed_vd(void)
+{
+	md_LED_PORT_u8 |= _BV(md_LED_PIN_u8);
+}
+
+static inline void m_ClearLed_vd(void)
+{
+	md_LED_PORT_u8 &= ~_BV(md_LED_PIN_u8);
+}
+
+int main(void)
+{
+	m_InitLed_vd();
+
+	while(true)
+	{
+		m_SetLed_vd();
+		_delay_ms( (double)md_WAIT_TIME_u32);
+		m_ClearLed_vd();
+		_delay_ms( (double)md_WAIT_TIME_u32 );
+	}
+
 	return 0;
 }
